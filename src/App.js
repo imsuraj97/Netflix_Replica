556662d5711059e967dce38a5a5a079506ec6e79
imsuraj97 <44128879+imsuraj97@users.noreply.github.com>
2021-03-19 14:25:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './Components/Hello'
+import Netflix from './Components/Netflix'
+
+
+function App() {
+  return (
+    <div className="App">
+      {/*<Hello />
+      */}
+      <Netflix />
+    </div>
+  );
+}
+
+export default App;
